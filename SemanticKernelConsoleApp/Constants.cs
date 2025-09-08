a993c02df98ce5361a9d3e95f58dc8bf173a934d
Marco Minerva <marco.minerva@gmail.com>
2025-09-08 11:37:51 +0200

    Add initial project structure and Q&A functionality
    Created a new Visual Studio solution file for the SemanticKernelConsoleApp, including project references and configurations. Added a `Constants.cs` file with constant values for API integration. Updated `Program.cs` to implement a Q&A loop using Semantic Kernel, with Azure OpenAI chat completion and logging. Established project file settings in `SemanticKernelConsoleApp.csproj` for SDK, output type, and package references.

---
+++ SemanticKernelConsoleApp/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace SemanticKernelConsoleApp;
+
+internal class Constants
+{
+    public const string Endpoint = "";
+    public const string ApiKey = "";
+    public const string Model = "";
+}
---
+++ SemanticKernelConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using SemanticKernelConsoleApp;
+
+var builder = Kernel.CreateBuilder();
+
+builder.Services.AddLogging(builder => builder.AddConsole());
+builder.Services.AddAzureOpenAIChatCompletion(Constants.Model, Constants.Endpoint, Constants.ApiKey);
+
+var kernel = builder.Build();
+
+var aiRequestSettings = new AzureOpenAIPromptExecutionSettings
+{
+    MaxTokens = 400,
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+// Q&A loop
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+var history = new ChatHistory();
+
+while (true)
+{
+    Console.Write("Question: ");
+
+    var question = Console.ReadLine();
+    history.AddUserMessage(question);
+
+    var answer = new StringBuilder();
+    await foreach (var token in chat.GetStreamingChatMessageContentsAsync(history, aiRequestSettings, kernel))
+    {
+        Console.Write(token);
+        answer.Append(token);
+
+        await Task.Delay(10);
+    }
+
+    history.AddAssistantMessage(answer.ToString());
+
+    Console.WriteLine();
+    Console.WriteLine();
+}
